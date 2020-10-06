ac4e27fb43179bc43de05cca1b85f5736fede00c
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2020-10-06 22:02:32 +0300

    Add files via upload

---
+++ problema.8.py
@@ -0,0 +1,13 @@
+a=int (input("prima cifra="))
+b=int (input("a doua cifra="))
+c=int (input("a treia cifra="))
+print(a*100+b*10+c,a*100+c*10+b,b*100+a*10+c,b*100+c*10+a,c*100+b*10+a,sep=' ') 
+
+
+
+
+
+
+
+
+
