0ec6de493607c2710795dd2f714ed4dd14b76979
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2020-10-06 21:52:34 +0300

    Add files via upload

---
+++ problema.1.py
@@ -0,0 +1,16 @@
+a=int(input("a="))
+if a == 1 :
+    print("Rosu")
+elif a== 2:
+ print("oranj")  
+elif a== 3:
+ print("Galben")  
+elif a== 4: 
+ print("Verde")  
+elif a== 5:
+ print("Albastru")  
+elif a== 6:
+ print("Indigo")  
+elif a== 7:
+ print("Violet")  
+else: print("invata culorile")
